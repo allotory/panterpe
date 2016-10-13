4cb32acff56b60ed4fa077e9daeb901451333779
Ellery Zhang <allotory@msn.com>
2016-10-13 20:56:06 +0800

    Add: add popup page

--- translater/js/common.js
+++ translater/js/common.js
@@ -1,15 +1,14 @@
 // 获取页面字符
 
-$(function(){
-    $("*").mouseup(function (e) {
-        var txt;
-        var parentOffset = $(this).offset();
-        var x = e.pageX - parentOffset.left;
-        var y = e.pageY - parentOffset.top;
-        txt = window.getSelection();
-        if (txt.toString().length > 1) {
-            alert(txt);
-        }
-    });
-    // alert("ready");
-});
+// $(function(){
+//     $("*").mouseup(function (e) {
+//         var txt;
+//         var parentOffset = $(this).offset();
+//         var x = e.pageX - parentOffset.left;
+//         var y = e.pageY - parentOffset.top;
+//         txt = window.getSelection();
+//         if (txt.toString().length > 1) {
+//             alert(txt);
+//         }
+//     });
+// });
